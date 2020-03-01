ac6aad4a51cbb3b452d5a5d74acdd1746e72d333
Sahil Jain <sahil.jain5125@gmail.com>
2020-02-29 16:16:27 -0800

    inference

---
+++ InferenceScript.py
@@ -0,0 +1,46 @@
+import tensorflow as tf
+import keras
+import numpy as np
+
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+
+from keras.layers import Input, Embedding, Bidirectional, Dense, GaussianNoise, Dropout, LSTM
+from keras.models import Model
+
+tokenizer = None
+max_len = 200
+
+with open('toxic-tokenizer.pickle','rb') as f:
+    #global tokeniz
+    tokenizer = pickle.load(f)
+
+vocab_size = 210444
+embedding_matrix = np.load('toxic-embed.npy')
+
+def civitas_model():
+    i = Input((200,))
+    e = Embedding(vocab_size,25,weights = [embedding_matrix],input_length=max_len ,trainable = False)(i)
+    #e = GaussianNoise(.05)(e)
+    b = Bidirectional(LSTM(256))(e)
+    b = Dropout(.3)(b)
+    d1 = Dense(128,activation = 'relu')(b)
+    d1 = Dense(128,activation = 'relu')(d1)
+    d1 = Dense(128,activation = 'relu')(d1)
+    d2 = Dense(2,activation = 'softmax')(d1)
+
+    return keras.models.Model(inputs = i, outputs = d2)
+
+model = civitas_model()
+model.summary()
+model.compile(optimizer='adam',loss='categorical_crossentropy',metrics = ['accuracy', 'categorical_crossentropy'])
+
+model.load_weights('civitas-2class.h5')
+
+def inferToxic(text):
+    seq = tokenizer.texts_to_sequences(text)
+
+    max_len = 200
+    pad_seq = pad_sequences(seq,maxlen=max_len)
+
+    return (model.predict(pad_seq)[0][1])
